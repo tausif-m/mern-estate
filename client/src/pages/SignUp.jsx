2707bce59d74775036528ce5d8f5ed84898e7cac
Tausif Mukhtar <email>
2024-01-24 18:37:04 +0530

    Created Sign Up Page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function SignUp() {
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
       <form className="flex flex-col gap-4">
         <input
           type="text"
