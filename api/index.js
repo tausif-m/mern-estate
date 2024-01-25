00e4534d1d79c8c71cf1466cb54e6a7fbe24d554
Tausif Mukhtar <email>
2024-01-25 11:52:43 +0530

    Created Sign In API Route

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
@@ -15,6 +16,8 @@ mongoose
   });
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
