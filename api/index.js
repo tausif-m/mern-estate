785234e44d86527fe53d14c7f0ddb8cf5d67dda6
Tausif Mukhtar <email>
2024-02-01 18:59:18 +0530

    Created User Update API

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose
@@ -32,6 +33,8 @@ app.use(function (req, res, next) {
 // app.use(cors());
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
